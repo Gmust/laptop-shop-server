bad5df14b459dee5f8eceeb06889839f13310606
Gmust <4orgood@gmail.com>
2023-05-08 18:53:00 +0200

    Create session login, started work on swagger docs

---
+++ src/config/configuration.ts
@@ -0,0 +1,8 @@
+import { registerAs } from '@nestjs/config';
+import { sqlConfig } from './sql.config';
+
+export const dbConfig = registerAs('database', () => ({
+  sql: {
+    ...sqlConfig(),
+  },
+}));
